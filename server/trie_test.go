12ef538855d90564c6ae0e994416259e73343f7e
cherrot <cherrotluo@gmail.com>
2019-01-26 12:51:26 +0800

    Feature: Add domain trie and its unittests.

---
+++ server/trie_test.go
@@ -0,0 +1,72 @@
+package server
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTrieNormalize(t *testing.T) {
+	trie := new(domainTrie)
+	trie.Add("")
+	if trie.end || trie.children != nil {
+		t.Error("Adding an empty string as domain name should have no effect")
+	}
+
+	trie2 := new(domainTrie)
+	trie.Add("google.com.")
+	trie2.Add("google.com")
+	if !reflect.DeepEqual(trie, trie2) {
+		t.Error("Domain name normalization failed")
+	}
+}
+
+func TestTrieAdd(t *testing.T) {
+	trie := new(domainTrie)
+	trie.Add("google.com")
+	trie.Add(".")
+	trie.Add("goo.gl")
+
+	if !(trie.end && trie.children == nil) {
+		t.Error("A dot should end at root.")
+	}
+	if !trie.Contain("www.google.com") || !trie.Contain("ietf.org") {
+		t.Error("A dot should contain any domain name")
+	}
+}
+
+func TestTrieContain(t *testing.T) {
+	trie := new(domainTrie)
+	if trie.Contain("goo.gl") {
+		t.Error("An empty trie contains nothing")
+	}
+
+	trie.Add("google.com")
+	trie.Add("api.github.com")
+	trie.Add("cn.")
+
+	if trie.Contain("google.com.") {
+		t.Error("`google.com.` should not be normalized to `google.com`")
+	}
+	if trie.Contain("www.github.com") {
+		t.Error("api.github.com should not contain www.github.com")
+	}
+	if trie.Contain("github.com") {
+		t.Error("api.github.com should not contain github.com")
+	}
+	if trie.Contain("ietf.org") || trie.Contain("twitter.com") {
+		t.Error("What a shit have you written?")
+	}
+
+	if !trie.Contain("google.com") {
+		t.Error("google.com should contain itself")
+	}
+	if !trie.Contain("api.github.com") {
+		t.Error("api.github.com should contain itself")
+	}
+	if !trie.Contain("www.google.com") || !trie.Contain("mail.google.com") {
+		t.Error("www.google.com and mail.google.com should be contained by google.com")
+	}
+	if !trie.Contain("12306.cn") {
+		t.Error("cn should contain all .cn domains")
+	}
+}
